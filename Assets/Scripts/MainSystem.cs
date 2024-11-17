0ae602130a826b12cb1db430e98a1e2a03f24ddb
jaxon <jaximus808@gmail.com>
2024-11-17 14:00:42 -0600

    popup window

--- Assets/Scripts/MainSystem.cs
+++ Assets/Scripts/MainSystem.cs
@@ -1,33 +1,29 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections; 
+
+using UnityEngine.UI;
+using TMPro;
 public class MainSystem : MonoBehaviour
 {
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject debug_swag; 
+    // Start is called once befo
+    public Transform reference;
+
+    public GameObject popup;
+
+    private GameObject current_popup; 
+    
+    public string DebugText; 
 
 
     public void buttonClicked()
     {
-        StartCoroutine(GetRequest("http://172.27.28.151:3000/test")); 
-
-    }
-
-    private IEnumerator GetRequest(string URL)
-    {
-        using (UnityWebRequest res = UnityWebRequest.Get(URL))
+        if(current_popup != null)
         {
-            yield return res.SendWebRequest();
-
-            if(res.result == UnityWebRequest.Result.Success)
-            {
-                this.debug_swag.transform.position += new Vector3(0.0f, 0.5f, 0.0f);
-            }
-            else 
-            {
-                this.debug_swag.transform.position -= new Vector3(0.0f, -0.5f, 0.0f);
-            }
+            Destroy(current_popup);
         }
+        current_popup = Instantiate(popup, reference.position, reference.rotation);
+        current_popup.GetComponentInChildren<TextMeshProUGUI>().text = DebugText;
     }
 }

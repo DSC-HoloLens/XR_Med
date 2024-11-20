9839096a1a6be783115d0dd4b4b7030855fd3745
ltahlia <l.tahlia@wustl.edu>
2024-11-19 18:17:33 -0600

    Changes to Mainsystem.cs

--- Assets/Scripts/MainSystem.cs
+++ Assets/Scripts/MainSystem.cs
@@ -19,11 +19,34 @@ public class MainSystem : MonoBehaviour
 
     public void buttonClicked()
     {
+       
+    }
+
+    void Start() {
         if(current_popup != null)
         {
             Destroy(current_popup);
         }
         current_popup = Instantiate(popup, reference.position, reference.rotation);
         current_popup.GetComponentInChildren<TextMeshProUGUI>().text = DebugText;
+        StartCoroutine(GetRequest("http://172.27.29.217:8000/medical-term"));
+
     }
+
+    IEnumerator GetRequest(string uri) {
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)){
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if(webRequest.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+                //Debug.LogError("Error", webRequest);
+            }
+            else{
+                Debug.Log("Error");
+            }
+        }
+    }   
 }
